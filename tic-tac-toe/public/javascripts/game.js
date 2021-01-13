8052b99af18bb5d8c701cad81227b50c30ecd9c6
Brandon Steigerwald <>
2021-01-13 07:58:59 -0600

    registered click handlers on all the squares

--- tic-tac-toe/public/javascripts/game.js
+++ tic-tac-toe/public/javascripts/game.js
@@ -1,3 +1,22 @@
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded!");
+  let marks = {
+    X: "tic-tac-toe/public/images/x.png",
+    O: "tic-tac-toe/public/images/o.png",
+  };
+  let startingPlayer = "X";
+  let turn = 0; //evens will be Starting Player
 
+  //register event listener on all squares
+  let squareElements = document.getElementsByClassName("square");
+  for (let element of Array.from(squareElements)) {
+    console.log("Square", element);
+    element.addEventListener("click", (clickEvent) => {
+      squareClickHandler(clickEvent);
+    });
+  }
 });
+
+function squareClickHandler(clickEvent) {
+  console.log(`Click on square ${clickEvent.path[0].id}: `, clickEvent);
+}
