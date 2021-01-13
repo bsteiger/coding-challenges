ce015ee4cd772c4b920a27a896ac09125bf26bf0
Brandon Steigerwald <>
2021-01-13 10:22:59 -0600

    append img if div is clicked.

--- tic-tac-toe/public/javascripts/game.js
+++ tic-tac-toe/public/javascripts/game.js
@@ -1,22 +1,38 @@
+let marks = {
+  X: "images/x.png",
+  O: "images/o.png",
+};
+let turn;
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("DOMContentLoaded!");
-  let marks = {
-    X: "tic-tac-toe/public/images/x.png",
-    O: "tic-tac-toe/public/images/o.png",
-  };
-  let startingPlayer = "X";
-  let turn = 0; //evens will be Starting Player
+  turn = 0;
 
   //register event listener on all squares
   let squareElements = document.getElementsByClassName("square");
   for (let element of Array.from(squareElements)) {
-    console.log("Square", element);
     element.addEventListener("click", (clickEvent) => {
       squareClickHandler(clickEvent);
     });
   }
 });
 
+/**
+ * Things that happen when a square is clicked.
+ * @param {Event} clickEvent
+ */
 function squareClickHandler(clickEvent) {
-  console.log(`Click on square ${clickEvent.path[0].id}: `, clickEvent);
+  console.log(
+    `Turn ${turn}: Click on square ${clickEvent.target.id}: `,
+    clickEvent
+  );
+
+  let imgSrc = turn % 2 ? marks.X : marks.O;
+  // remove innerhtml
+  clickEvent.innerHTML = "";
+
+  //add png to the square
+  let element = document.createElement("img");
+  element.setAttribute("src", imgSrc);
+  element.setAttribute("height", 100);
+  clickEvent.target.append(element);
+  turn += 1;
 }
