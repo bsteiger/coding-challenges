f1dd0a7bb7d430d6a651bf3c573f824feb14934c
Brandon Steigerwald <>
2021-01-13 20:00:43 -0600

    renamed squareClickHandler -> onSquareClick add game-mark class to piece png elements

--- tic-tac-toe/public/javascripts/game.js
+++ tic-tac-toe/public/javascripts/game.js
@@ -3,15 +3,14 @@ let marks = {
   O: "images/o.png",
 };
 let turn;
+
 window.addEventListener("DOMContentLoaded", () => {
   turn = 0;
 
   //register event listener on all squares
   let squareElements = document.getElementsByClassName("square");
   for (let element of Array.from(squareElements)) {
-    element.addEventListener("click", (clickEvent) => {
-      squareClickHandler(clickEvent);
-    });
+    element.addEventListener("click", onSquareClick);
   }
 });
 
@@ -19,20 +18,21 @@ window.addEventListener("DOMContentLoaded", () => {
  * Things that happen when a square is clicked.
  * @param {Event} clickEvent
  */
-function squareClickHandler(clickEvent) {
+function onSquareClick(clickEvent) {
   console.log(
     `Turn ${turn}: Click on square ${clickEvent.target.id}: `,
     clickEvent
   );
 
-  let imgSrc = turn % 2 ? marks.X : marks.O;
+  let imgSrc = turn % 2 ? marks.O : marks.X;
   // remove innerhtml
-  clickEvent.innerHTML = "";
+  clickEvent.target.innerHTML = "";
 
   //add png to the square
   let element = document.createElement("img");
   element.setAttribute("src", imgSrc);
   element.setAttribute("height", 100);
+  element.classList.add("game-mark");
   clickEvent.target.append(element);
   turn += 1;
 }
