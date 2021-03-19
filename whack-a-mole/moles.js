8efaf897176d372b4b7a54fc4978ff4c76f1a52f
Brandon Steigerwald <bmsteiger@gmail.com>
2021-03-19 00:34:50 -0500

    One pop up per start click

--- whack-a-mole/moles.js
+++ whack-a-mole/moles.js
@@ -1,3 +1,58 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("Hi i loaded.");
+  //Register a click action on the start button.  Use that to start the game
+  let startBtn = document.getElementById("startBtn");
+  startBtn.addEventListener("click", startGame);
 });
+
+/** Returns a random integer between and up to start and end values */
+function randomInt(start, end) {
+  return Math.floor(Math.random() * end + start);
+}
+
+function startGame() {
+  //make a random mole appear for a random time
+  popUpMole();
+}
+
+/** Pop up a random mole for a random time after a random delay */
+async function popUpMole() {
+  let minTimeUp = 300;
+  let maxTimeUp = 1000;
+  const upDelay = randomInt(0, 2000);
+  let upTime = randomInt(minTimeUp, maxTimeUp);
+  let hole = randomInt(1, 6);
+
+  console.log(
+    `Going to pop up hole ${hole} for ${upTime}ms after a ${upDelay}ms delay`
+  );
+
+  await sleep(upDelay);
+  holeUp(hole);
+  setTimeout(() => {
+    holeDown(hole);
+  }, upTime);
+}
+
+/** Sleep Function for async timeouts */
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+/**
+ * Set a hole to be up
+ * @param {Number} hole
+ */
+function holeUp(hole) {
+  let element = document.getElementsByClassName(`hole${hole}`);
+  element[0].classList.add("up");
+}
+
+/**
+ * Set a hole to be down
+ * @param {Number} hole
+ */
+function holeDown(hole) {
+  let element = document.getElementsByClassName(`hole${hole}`);
+  element[0].classList.remove("up");
+}
